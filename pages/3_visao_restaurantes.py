1cc217e491f9bd6e3f516a26123ad2af6737082a
Dérek Augusto <54606382+Derek-Augusto@users.noreply.github.com>
2023-06-27 15:57:31 -0300

    Update 3_visao_restaurantes.py
    adicionei import datetime
    E o comando to_ aos códigos pd.datetime

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -20,6 +20,8 @@ import folium
 
 import numpy as np
 
+import datetime
+
 st.set_page_config( page_title = 'Visão Restaurantes', page_icon='🍽️', layout='wide')
 
 
@@ -172,7 +174,7 @@ st.sidebar.markdown('## Fastest Delivery in Town')
 st.sidebar.markdown('---')
 
 st.sidebar.markdown('##  Selecione uma data limite')
-date_slider = st.sidebar.slider ('Até qual valor?', value=pd.datetime(2022,4,13), min_value=pd.datetime(2022,2,11), max_value=pd.datetime(2022,4,6), format='DD-MM-YYYY')
+date_slider = st.sidebar.slider ('Até qual valor?', value=pd.to_datetime(2022,4,13), min_value=pd.to_datetime(2022,2,11), max_value=pd.to_datetime(2022,4,6), format='DD-MM-YYYY')
 
 
 st.header ( date_slider)
